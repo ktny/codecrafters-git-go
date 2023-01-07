d4b12efdad8d3d0c8e86a561f39c52f780aa82dd
ktny <katsunori.t10@gmail.com>
2023-01-07 21:28:57 +0900

    cat file command

---
+++ cmd/mygit/init_command.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func initCmd() error {
+	for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+		}
+	}
+
+	headFileContents := []byte("ref: refs/heads/master\n")
+	if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+	}
+
+	fmt.Println("Initialized git directory")
+
+	return nil
+}
--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -12,23 +12,19 @@ func main() {
 		os.Exit(1)
 	}
 
+	var err error
+
 	switch command := os.Args[1]; command {
 	case "init":
-		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
-			if err := os.MkdirAll(dir, 0755); err != nil {
-				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
-			}
-		}
-
-		headFileContents := []byte("ref: refs/heads/master\n")
-		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
-		}
-
-		fmt.Println("Initialized git directory")
-
+		err = initCmd()
+	case "cat-file":
+		err = catFileCmd(os.Args[1:])
 	default:
-		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
+		err = fmt.Errorf("Unknown command %s", command)
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err.Error())
 		os.Exit(1)
 	}
 }
